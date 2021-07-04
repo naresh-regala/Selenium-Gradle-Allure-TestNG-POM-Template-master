dc8000051b0ba4dc19956521fc922083491a240c
Naresh Kumar Regala <nregala@opentext.com>
2021-07-04 14:52:22 +0530

    removing parallel="methods" because by default azure does nt support parallelism

--- src/main/java/listeners/ConfigureSuite.java
+++ src/main/java/listeners/ConfigureSuite.java
@@ -11,7 +11,7 @@ public class ConfigureSuite implements IAlterSuiteListener {
 
 	@Override
 	public void alter(List<XmlSuite> suites) {
-		int count = Integer.parseInt(System.getProperty("threadcount", "3"));
+		int count = Integer.parseInt(System.getProperty("threadcount", "2"));
 		Logg.info("Configured number of parallel threads: " + count);
 		XmlSuite suite = suites.get(0);
 		suite.setDataProviderThreadCount(count);
